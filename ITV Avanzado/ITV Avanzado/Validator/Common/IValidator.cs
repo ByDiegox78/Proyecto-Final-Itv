a95732e706af43346fd07d8b440445ff02b7e6f6
ByDiegox78 <oms04081974@gmail.com>
2026-04-16 17:00:24 +0200

    Interfaz validador

---
+++ ITV Avanzado/ITV Avanzado/Validator/Common/IValidator.cs
@@ -0,0 +1,8 @@
+﻿using CSharpFunctionalExtensions;
+using ITV_Avanzado.Error.Common;
+
+namespace ITV_Avanzado.Validator.Common;
+
+public interface IValidator<T> {
+    Result<T, DomainError> Validar(T entity);
+}
