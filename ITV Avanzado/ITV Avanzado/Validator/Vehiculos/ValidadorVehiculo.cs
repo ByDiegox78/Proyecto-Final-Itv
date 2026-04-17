b654fe2f7f3a37c0770a52e2686fdaf362224ec9
ByDiegox78 <oms04081974@gmail.com>
2026-04-17 13:31:43 +0200

    Test validador

---
+++ ITV Avanzado/ITV Avanzado/Validator/Vehiculos/ValidadorVehiculo.cs
@@ -0,0 +1,57 @@
+﻿    using System.Text.RegularExpressions;
+    using CSharpFunctionalExtensions;
+    using GestionItv.Models;
+    using ITV_Avanzado.Error.Common;
+    using ITV_Avanzado.Error.Vehiculos;
+    using ITV_Avanzado.Validator.Common;
+    using static System.Console;
+
+    namespace ITV_Avanzado.Validator.Vehiculos;
+
+    public class ValidadorVehiculo : IValidator<Vehiculo> {
+        public Result<Vehiculo, DomainError> Validar(Vehiculo entidad) {
+            var errores = new List<string>();
+            
+            if (!RegexMatricula.IsMatch(entidad.Matricula)) {
+                errores.Add("La matricula no cumple la regla de NNNNLLL");
+                
+            }
+            if (string.IsNullOrWhiteSpace(entidad.Marca) || entidad.Marca.Length < 2) {
+                errores.Add("La marca debe contener al manos 2 carazteres");
+                
+            }
+            bool estaEnRangoValido = entidad.Cilindrada >= 800 && entidad.Cilindrada <= 3000;
+            if (entidad.TipoMotor == Motor.Electrico) {
+                if (entidad.Cilindrada != 0)
+                    errores.Add("La cilindrada debe ser 0 en vehículos eléctricos");
+            } else {
+                if (!estaEnRangoValido)
+                    errores.Add("La cilindrada debe estar entre 800 y 3000 (excepto vehículos eléctricos)");
+            }
+
+            if (!Enum.IsDefined(entidad.TipoMotor)) {
+                errores.Add("El tipo de motor no coincide con los disponibles");
+            }
+
+            if (!ValidarDni(entidad.DniPropietario)) {
+                errores.Add("El dni del dueño no tiene el formato correcto");
+            }
+
+            if (errores.Any())
+                return Result.Failure<Vehiculo, DomainError>(VehiculoErrors.Validation(errores));
+            return Result.Success<Vehiculo, DomainError>(entidad);
+        }
+        private bool ValidarDni(string dni) {
+            string letrasDniPermitidas = "TRWAGMYFPDXBNJZSQVHLCKE";
+            if (string.IsNullOrWhiteSpace(dni) || !RegexDni.IsMatch(dni))
+                return false;
+            int numero = int.Parse(dni.Substring(0, 8));
+            char letraCorrecta = letrasDniPermitidas[numero % 23];
+            return dni[8] == letraCorrecta;
+        }
+        private static readonly Regex RegexMatricula =
+            new(@"^[0-9]{4}[BCDFGHJKLMNPRSTVWXYZ]{3}$");
+        
+        private static readonly Regex RegexDni =
+            new(@"^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$");
+    }
---
+++ ITV Avanzado/ITV Test/Validator/ValidadorVehiculoTest.cs
@@ -0,0 +1,156 @@
+﻿using FluentAssertions;
+using GestionItv.Models;
+using ITV_Avanzado.Error.Vehiculos;
+using ITV_Avanzado.Validator.Vehiculos;
+
+namespace ITV_Test.Validator;
+
+[TestFixture]
+public class ValidadorVehiculoTest {
+    [TestFixture]
+    public class CasosPositivos {
+        [SetUp]
+        public void SetUp() {
+            _validador = new ValidadorVehiculo();
+        }
+        private ValidadorVehiculo _validador = null!;
+        [Test]
+        public void Validar_VehiculoValido_Succes() {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = "Fiat", Cilindrada = 900, TipoMotor = Motor.Diesel,
+                DniPropietario = "54407737H",
+            };
+            var res = _validador.Validar(vehiculo);
+
+            res.IsSuccess.Should().BeTrue();
+        }
+        [TestCase(Motor.Diesel)]
+        [TestCase(Motor.Hibrido)]
+        [TestCase(Motor.Gasolina)]
+        public void Validate_MotoresCombustionValidos_Succes(Motor motor) {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = "Fiat", Cilindrada = 900, TipoMotor = motor,
+                DniPropietario = "54407737H",
+            };
+            var res = _validador.Validar(vehiculo);
+
+                res.IsSuccess.Should().BeTrue();
+                res.Value.TipoMotor.Should().Be(motor);
+        }
+        [TestCase(Motor.Electrico)]
+        public void Validate_MotoreElectricoValido_Succes(Motor motor) {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = "Fiat", Cilindrada = 0, TipoMotor = motor,
+                DniPropietario = "54407737H",
+            };
+            var res = _validador.Validar(vehiculo);
+
+            res.IsSuccess.Should().BeTrue();
+            res.Value.TipoMotor.Should().Be(motor);
+        }
+    }
+
+    [TestFixture]
+    public class CasosInvalidos {
+        [SetUp]
+        public void SetUp() {
+            _validador = new ValidadorVehiculo();
+        }
+        private ValidadorVehiculo _validador = null!;
+
+        [TestCase("AAAAAAA")]
+        [TestCase("123RTG")]
+        [TestCase("FMR4196")]
+        [TestCase("FM4196R")]
+        public void Validar_MatriculaInvalida_Invalid(string matricula) {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = matricula, Marca = "Fiat", Cilindrada = 900, TipoMotor = Motor.Diesel,
+                DniPropietario = "54407737H",
+            };
+
+            var res = _validador.Validar(vehiculo);
+
+            res.IsFailure.Should().BeTrue();
+            res.Error.Should().BeOfType<VehiculoError.Validation>();
+            (res.Error as VehiculoError.Validation)?.Errors.Should()
+                .Contain("La matricula no cumple la regla de NNNNLLL");
+        }
+
+        [TestCase("A")]
+        [TestCase("")]
+        public void Validar_MarcaInvalida_NoSucces(string marca) {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = marca, Cilindrada = 900, TipoMotor = Motor.Diesel,
+                DniPropietario = "54407737H",
+            };
+            var res = _validador.Validar(vehiculo);
+            
+            res.IsFailure.Should().BeTrue();
+            res.Error.Should().BeOfType<VehiculoError.Validation>();
+            (res.Error as VehiculoError.Validation)?.Errors.Should()
+                .Contain("La marca debe contener al manos 2 carazteres");
+        }
+        [TestCase(200)]
+        [TestCase(3500)]
+        [TestCase(1)]
+        public void Validar_CilindradaElectricoInvalida_NoSucces(int cilindrada) {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = "Fiat", Cilindrada = 900, TipoMotor = Motor.Electrico,
+                DniPropietario = "54407737H",
+            };
+            var res = _validador.Validar(vehiculo);
+            
+            res.IsFailure.Should().BeTrue();
+            res.Error.Should().BeOfType<VehiculoError.Validation>();
+            (res.Error as VehiculoError.Validation)?.Errors.Should()
+                .Contain("La cilindrada debe ser 0 en vehículos eléctricos");
+        }
+        [TestCase(200)]
+        [TestCase(3500)]
+        [TestCase(0)]
+        public void Validar_CilindradaCombustionInvalida_NoSucces(int cilindrada) {
+            
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = "Fiat", Cilindrada = cilindrada, TipoMotor = Motor.Diesel,
+                DniPropietario = "54407737H",
+            };
+            var res = _validador.Validar(vehiculo);
+            
+            res.IsFailure.Should().BeTrue();
+            res.Error.Should().BeOfType<VehiculoError.Validation>();
+            (res.Error as VehiculoError.Validation)?.Errors.Should()
+                .Contain("La cilindrada debe estar entre 800 y 3000 (excepto vehículos eléctricos)");
+        }
+
+        [Test]
+        public void Validar_MotorInvalido_NoSucces() {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = "Fiat", Cilindrada = 900, TipoMotor = (Motor)1000,
+                DniPropietario = "54407737H",
+            };
+            var res = _validador.Validar(vehiculo);
+            
+            res.IsFailure.Should().BeTrue();
+            res.Error.Should().BeOfType<VehiculoError.Validation>();
+            (res.Error as VehiculoError.Validation)?.Errors.Should()
+                .Contain("El tipo de motor no coincide con los disponibles");
+        }
+        [TestCase("RRRRRRR")]
+        [TestCase("R5673456")]
+        [TestCase("234R789")]
+        [TestCase("54407737z")]
+        public void Validar_DniInvalida_NoSucces(string dni) {
+            var vehiculo = new Vehiculo {
+                Id = 1, Matricula = "4196FMR", Marca = "Fiat", Cilindrada = 900, TipoMotor = Motor.Diesel,
+                DniPropietario = dni,
+            };
+            var res = _validador.Validar(vehiculo);
+            
+            res.IsFailure.Should().BeTrue();
+            res.Error.Should().BeOfType<VehiculoError.Validation>();
+            (res.Error as VehiculoError.Validation)?.Errors.Should()
+                .Contain("El dni del dueño no tiene el formato correcto");
+        }
+    }
+    
+}
