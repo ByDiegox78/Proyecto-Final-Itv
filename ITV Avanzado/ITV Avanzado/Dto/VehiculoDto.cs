67c272110734b0f331f6f700b82d9fa9b824d79e
ByDiegox78 <oms04081974@gmail.com>
2026-04-18 12:54:04 +0200

    Clase dto completada

---
+++ ITV Avanzado/ITV Avanzado/Dto/VehiculoDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Serialization;
+
+namespace ITV_Avanzado.Dto;
+
+[XmlRoot("Vehiculo")]
+[XmlType("VehiculoDto")]
+public record VehiculoDto(
+    [property: XmlElement("Id")] int Id,
+    [property: XmlElement("Matricula")] string Matricula,
+    [property: XmlElement("Marca")] string Marca,
+    [property: XmlElement("Cilindrada")] int Cilindrada,
+    [property: XmlElement("TipoMotor")] string TipoMotor,
+    [property: XmlElement("DniPropietario")]
+    string DniPropietario,
+    [property: XmlElement("IsDelete")] bool IsDelete,
+    [property: XmlElement("CreatedAt")] string CreatedAt,
+    [property: XmlElement("UpdatedAt")] string UpdatedAt
+) {
+    public VehiculoDto() : this(
+        0,          
+        "",        
+        "",
+        0,          
+        "",        
+        "",         
+        false,      
+        "",         
+        ""          
+    )
+    { }
+}
