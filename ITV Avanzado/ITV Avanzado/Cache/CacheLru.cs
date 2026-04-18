4c3d249e9feffedfd82d47b0aa248243c9397230
ByDiegox78 <oms04081974@gmail.com>
2026-04-18 18:02:46 +0200

    Clase cache y metodo add

---
+++ ITV Avanzado/ITV Avanzado/Cache/CacheLru.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+
+namespace ITV_Avanzado.Cache;
+
+public class CacheLru<TKey, TValue> : ICache<TKey, TValue>
+    where TKey : notnull {
+    private readonly int _capacity;
+    private readonly Dictionary<TKey, TValue> _data = new();
+    private readonly LinkedList<TKey> _usageOrder = new();
+    private readonly ILogger _logger = Log.ForContext<CacheLru<TKey, TValue>>();
+
+    public CacheLru(int capacity) {
+        if (capacity <= 0)
+            throw new ArgumentException("La capacidad no puede ser menor o igual que 0", nameof(capacity));
+        _capacity = capacity;
+    }
+    /// <inheritdoc cref="ICache{TKey,TValue}.Add" />
+    public void Add(TKey key, TValue value) {
+        _logger.Debug("Añadiendo clave: {Key}", key);
+        if (_data.TryGetValue(key, out var existingValue)) {
+            _logger.Debug("La Clave {Key} ya existe. Actualizando el valor: {Old} a {New}", key, existingValue, value);
+            _data[key] = value;
+            RefreshUsage(key);
+            return;
+        }
+        _logger.Debug("La Clave {Key} es nueva. La Capacidad Actual de la cache es de: {Used}/{Total}", 
+            key, _data.Count, _capacity);
+        if (_data.Count >= _capacity) {
+            var oldestKey = _usageOrder.First!.Value;
+            var oldestValue = _data[oldestKey];
+            _logger.Debug("La cache esta llena. Sacando el elemento: {Key} = {Value}",
+                oldestKey, oldestValue);
+            _usageOrder.RemoveFirst();
+            _data.Remove(oldestKey);
+        }
+        _data.Add(key, value);
+        _usageOrder.AddLast(key);
+        _logger.Debug("Se añadio el elemnto, La nueva cache es: {Order}",
+            string.Join(" -> ", _usageOrder));    }
+
+    public TValue? Get(TKey key) {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(TKey key) {
+        throw new NotImplementedException();
+    }
+}
