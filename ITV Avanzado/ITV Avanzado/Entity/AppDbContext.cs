564068472dca32183c1529d38a2f0a2fb6b72f2e
ByDiegox78 <oms04081974@gmail.com>
2026-04-18 13:18:06 +0200

    Clase entit y AppDbContext creadas

---
+++ ITV Avanzado/ITV Avanzado/Entity/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ITV_Avanzado.Entity;
+
+public class AppDbContext : DbContext {
+    public DbSet<VehiculoEntity> Vehiculos { get; set; } = null!;
+
+    private readonly string _connectionString;
+
+    public AppDbContext(string connectionString) {
+        _connectionString = "";
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder op) {
+        if (!op.IsConfigured) op.UseSqlite(_connectionString);
+    }
+    public void EnsurceCreated() {
+        Database.EnsureCreated();
+    }
+}
