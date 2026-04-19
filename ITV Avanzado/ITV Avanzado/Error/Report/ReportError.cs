a20f43d98823a9dfe1fa8bb879f28208cb67b81f
ByDiegox78 <oms04081974@gmail.com>
2026-04-19 13:34:59 +0200

    Conflicto resuelto y interfaz commun de estorage realizada

--- ITV Avanzado/ITV Avanzado/Error/Report/ReportError.cs
+++ ITV Avanzado/ITV Avanzado/Error/Report/ReportError.cs
@@ -1,22 +1,27 @@
 ﻿using ITV_Avanzado.Error.Common;
 
-namespace ITV_Avanzado.Error.Storage;
+namespace ITV_Avanzado.Error.Report;
 
 public abstract record ReportError(string Message) : DomainError(Message) {
     public sealed record GenerationError(string Details)
         : ReportError($"Error al generar el informe: {Details}");
-    public sealed record SaveError(string Details)
+
+    public sealed record SaveError(string Details)  
         : ReportError($"Error al guardar el informe: {Details}");
+
     public sealed record DirectoryError(string Details)
         : ReportError($"Error con el directorio de informes: {Details}");
 }
+
 public static class ReportErrors {
     public static DomainError GenerationError(string details) {
         return new ReportError.GenerationError(details);
     }
+
     public static DomainError SaveError(string details) {
         return new ReportError.SaveError(details);
     }
+
     public static DomainError DirectoryError(string details) {
         return new ReportError.DirectoryError(details);
     }
