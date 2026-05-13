e0d57972121af4d87f6f8300e3c45a67cf094d4a
ByDiegox78 <oms04081974@gmail.com>
2026-05-13 13:22:13 +0200

    Proyecto front y clase converters

---
+++ ITV Avanzado/ITV Avanzado.Front/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ITV_Avanzado.Front;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+}
